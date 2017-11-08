4c445308bd9250ede37f451381a1cfe6c54f2737
JunyaUSUI <junya.usui.0714@gmail.com>
2017-11-08 16:27:39 +0900

    Binary Search

---
+++ Search/Binary_Search.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int A[10000], n;
+
+int binarySearch(int key) {
+  int left = 0;
+  int right = n;
+  int mid = 0;
+  while( left < right ) {
+    mid = (left + right ) / 2;
+    if ( A[mid] == key ) return 1; // key found !
+    if ( key > A[mid] ) left = mid + 1;
+    else if ( key < A[mid] ) right = mid;
+  }
+  return 0;
+}
+
+int main() {
+  int i, q, k, sum = 0;
+ 
+  scanf("%d", &n);
+  for ( int i = 0; i < n; i++ ) {
+    scanf("%d", &A[i]);
+  }
+
+  scanf("%d", &q);
+  for ( i = 0; i < q; i++ ) {
+    scanf("%d", &k);
+    if ( binarySearch( k ) ) sum++;
+  }
+  printf("%d\n", sum);
+  
+  return 0;
+}
