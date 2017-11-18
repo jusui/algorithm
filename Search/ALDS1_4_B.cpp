f19e12890bb074b793aaa874e9023a6c23eb16b5
JunyaUSUI <junya.usui.0714@gmail.com>
2017-11-18 23:17:09 +0900

    Binary Search with STL::lower_bound()

---
+++ Search/ALDS1_4_B.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+
+int A[1000000], n;
+
+int main() {
+  scanf("%d", &n);
+  for ( int  i = 0; i < n; i++ ) {
+    scanf("%d", &A[i]);
+  }
+
+  int q, k, sum = 0;
+  scanf("%d", &q);
+  for ( int i = 0; i < q; i++ ) {
+    scanf("%d", &k);
+
+    // STL : lower_bound()
+    if ( *lower_bound(A, A + n, k) == k )
+      sum++;
+  }
+
+  cout << "sum = " << sum << endl;
+
+  return 0;
+}
+
+
