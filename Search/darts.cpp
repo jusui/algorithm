11ce45b6800832e7a13bdbc5785a2c887a1342da
JunyaUSUI <junya.usui.0714@gmail.com>
2018-03-03 05:45:01 +0900

    [Darts] try1

---
+++ Search/darts.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define rep(i,n) for(int i = 0; i < (n); i++)
+
+int main(void) {
+  while(1) {
+    int n, m; cin >> n >> m;
+    if ( n == 0 && m == 0 ) return 0;
+    vector<int> p(n);
+    rep(i, n) cin >> p[i];
+    p.push_back(0); // 矢を使わない場合
+    int ans = 0;
+    // 計算量(n^4)
+    rep(i, n + 1) rep(j, n + 1) rep(k, n + 1) rep(l, n + 1) {
+      int tmp = p[i] + p[j] + p[k] + p[l];
+      if (tmp <= m) ans = max(ans, tmp);
+    }
+    printf("%d\n", ans);
+  }
+  return 0;
+}
+
