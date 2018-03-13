002c30266d69385cf2c2234fd1551a5453c8343f
JunyaUSUI <junya.usui.0714@gmail.com>
2018-03-13 16:58:48 +0900

    no message

--- Search/darts.cpp
+++ Search/darts.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 #define rep(i,n) for(int i = 0; i < (n); i++)
 
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0529
+
 int main(void) {
   while(1) {
     int n, m; cin >> n >> m;
@@ -13,14 +15,21 @@ int main(void) {
     vector<int> p(n);
     rep(i, n) cin >> p[i];
     p.push_back(0); // 矢を使わない場合
-    sort(p.begin(), p.end());
+    vector<int> pp((n + 1) * (n + 1));
+    int cnt = 0;
+    rep(i, n + 1)rep(j, n + 1){
+      pp[cnt] = p[i] + p[j];
+      cnt++;
+    }
+    
+    sort(pp.begin(), pp.end());
     int ans = 0;
     // 計算量(n^3 * log(n))
-    rep(i, n + 1) rep(j, n + 1) rep(k, n + 1) {
-      int tmp = p[i] + p[j] + p[k];
+    rep(i, (n + 1) * (n + 1)) {
+      int tmp = pp[i];
       int aim = m - tmp;
       if ( aim < 0 ) continue;
-      tmp += *(upper_bound(p.begin(), p.end(), aim) - 1);
+      tmp += *(upper_bound(pp.begin(), pp.end(), aim) - 1);
       if (tmp <= m) ans = max(ans, tmp);
     }
     printf("%d\n", ans);
