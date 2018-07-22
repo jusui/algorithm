b997bb4b9643750e9c408b9e49ff4a5d3f2de121
JunyaUSUI <junya.usui.0714@gmail.com>
2018-07-22 18:23:51 +0900

    insertion sort

--- Sort/ALDS1_1_A.cpp
+++ Sort/ALDS1_1_A.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-void trace( int A[], int N) {
+/* Output of element in array */
+void trace(int A[], int N) {
   int i;
   for ( i = 0; i < N; i++ ) {
     if ( i > 0 ) cout << " " << endl;
@@ -11,16 +11,21 @@ void trace( int A[], int N) {
   printf("\n");
 }
 
+/* insertion Sort algorithm */
 void insertionSort(int A[], int N) {
-  int j, i, v;
+  /* A[N]:SizeがNの整数型配列
+     i:未ソート部分の先頭を示すループ変数
+     v:A[i]を一時的に保持する変数
+     j:Sort済み部分列からvを挿入するための位置を探すループ変数 */
+  int i, j, v; 
   for ( i = 0; i < N; i++ ) {
     v = A[i];
     j = i - 1;
-    while( j >= 0 && A[j] > v ) {
+    while ( j >= 0 && A[j] > v ) {
       A[j + 1] = A[j];
       j--;
     }
-    A[j+1]  = v;
+    A[j + 1] = v;
     trace(A, N);
   }
 }
@@ -31,7 +36,7 @@ int main() {
 
   scanf("%d", &N);
   for ( i = 0; i < N; i++ ) scanf("%d", &A[i]);
-
+  
   trace(A, N);
   insertionSort(A, N);
 
